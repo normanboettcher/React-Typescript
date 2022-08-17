8ba5097e0004234480cb1431f2168ac6aad2c065
normanboettcher <norman.boettcher@web.de>
2022-08-17 18:16:57 +0200

    added components

--- src/main/js/src/App.tsx
+++ src/main/js/src/App.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import HelloWorld from "components/HelloWorld";
+import "./style/App.css"
+import InputField from "components/InputField";
+import { Todo } from "model";
+import TodoList from "components/TodoList";
 
-const App = () => {
+const App : React.FC  = () => {
+
+    const [todo, setTodo] = useState<string>("");
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const handleAdd=(e: React.FormEvent) => {
+        e.preventDefault();
+
+        if(todo){
+            setTodos([...todos, {id:Date.now(), todo, isDone:false}]);
+            setTodo("");
+        }
+        
+    }
+
     return (
         <div className="App">
-            <HelloWorld/>
+            <span className="heading">Taskify</span>
+            <InputField todo = {todo} setTodo = {setTodo} handleAdd={handleAdd}/>
+            <TodoList todos = {todos} setTodos={setTodos}/>
+            
         </div>
     );
 }
